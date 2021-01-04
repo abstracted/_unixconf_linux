1d06a556f46376fb5de25edbc20d60ca8fc3274e
Cameron <csanders@protonmail.com>
2021-01-03 23:57:38 -0600

    update

--- run-playbook-mediaserver.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-# Dnscrypt proxy needs to come before mediaserver 
-# otherwise static network configs will be set before dns resover is setup
-./generate-config -s mediaserver -m os-manjaro,dnscrypt-proxy,mediaserver,docker-portainer,docker-swag,docker-nextcloud
-sudo ansible-playbook playbook.yml
