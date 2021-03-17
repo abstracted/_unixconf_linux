4975c6ddd94b8ee84e0c1a4d803f6672bec46359
Cameron <csanders@protonmail.com>
2021-03-17 17:18:22 -0500

    updates

--- src/includes/cli-options.js
+++ src/includes/cli-options.js
@@ -7,6 +7,7 @@ const { MODULE_PATH, SYSTEM_PATH } = require('./dictionary.js')
 program
   .option(`-m, --modules <modules>', 'Comma separated list of modules to add to the sytem configuration. Module configs are stored in ${MODULE_PATH}`, null, parseCommaSeparatedList, [])
   .option(`-s, --system <system>', 'Specific system to use. System configs are stored in ${SYSTEM_PATH}`)
+  .option('-t, --test', 'Test the configuration with vagrant')
   .parse()
 
 module.exports = program
