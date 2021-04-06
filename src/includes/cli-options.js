c3a444f33d1304a1f375dc47abe0deb08f569f7c
Cameron <csanders@protonmail.com>
2021-04-06 15:50:00 -0500

    make system a required option

--- src/includes/cli-options.js
+++ src/includes/cli-options.js
@@ -5,8 +5,8 @@ const { parseCommaSeparatedList } = require('./helpers')
 const { MODULE_PATH, SYSTEM_PATH } = require('./dictionary.js')
 
 program
+  .requiredOption(`-s, --system <system>', 'Specific system to use. System configs are stored in ${SYSTEM_PATH}`)
   .option(`-m, --modules <modules>', 'Comma separated list of modules to add to the sytem configuration. Module configs are stored in ${MODULE_PATH}`, null, parseCommaSeparatedList, [])
-  .option(`-s, --system <system>', 'Specific system to use. System configs are stored in ${SYSTEM_PATH}`)
   .option('-t, --test', 'Test the configuration with vagrant')
   .parse()
 
